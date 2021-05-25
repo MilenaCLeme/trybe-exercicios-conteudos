93494aa5b66c5c75076419ce213c0cc69d4f3cd5
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-25 16:29:31 -0300

    Exericio sete finalizado

--- bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -73,11 +73,11 @@ if (a + b + c === 180) {
 }
 
 //Exercicio 6
-let peçaDeXadrez = "Rei";
+let peçaDeXadrez = "Rainha";
 
 switch(peçaDeXadrez){
     case "Rei": 
-    console.log(peçaDeXadrez + "-> se movimenta para qualquer direção, uma casa for vezes" );
+    console.log(peçaDeXadrez + "-> se movimenta para qualquer direção, sendo uma casa for vezes" );
     break;
     case "Rainha":
     console.log(peçaDeXadrez + "-> se movimenta para qualquer direção, sendo a mais forte");
@@ -95,3 +95,24 @@ switch(peçaDeXadrez){
     console.log(peçaDeXadrez + "-> se movimenta na horizontal");
     break;
 }
+
+//Exercicio 7
+
+let nota = 101;
+
+if(nota >= 90 && nota <= 100){
+    console.log("A");
+} else if(nota >= 80 && nota < 90) {
+    console.log("B");
+} else if(nota >= 70 && nota < 80) {
+    console.log("C");
+} else if(nota >= 60 && nota < 70) {
+    console.log("D");
+} else if(nota >= 50 && nota < 60) {
+    console.log("E");
+} else if(nota < 50 && nota > 0) {
+    console.log("F");
+} else {
+    console.log("Mensagem de erro 001 - Numero invalido");
+}
+
