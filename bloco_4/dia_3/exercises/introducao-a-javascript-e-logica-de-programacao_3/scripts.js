5089b6b806fd1a194f867e511ed74169b8ac4ac0
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-25 15:26:01 -0300

    Iniciando exericios

---
+++ bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts.js
@@ -0,0 +1,26 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n 
+
+//programa que imprimi "*"
+let caract = "*"
+//repedi x vezes a coluna
+let nVezes = 5;
+
+
+for(let i = 0; i < nVezes; ++i){
+    let coluna = caract;
+}
+//repedi x vezes a linha
+let linha = 0;
+for(let index = 0; index < nVezes; ++index){
+    let resultado = 
+    console.log(resultado);
+}
+
+
+
+
+
+
+//for(let index = 0; index < n; ++index) {
+//   let coluna = n 
+//}
