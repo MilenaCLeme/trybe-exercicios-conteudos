f07e445fccbfd3a303c7c3f7c6ca5bff1d66a497
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-26 13:39:01 -0300

    Exercicio 1 do bloco 4 dia 3 Finalizado

--- bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts.js
+++ bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts.js
@@ -1,26 +1,17 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n 
 
-//programa que imprimi "*"
-let caract = "*"
-//repedi x vezes a coluna
-let nVezes = 5;
+//Qual a variavel que precisa imprimir 
+let caracteristica = ""
 
+//repedi x vezes a coluna 
+let nVezesDeRepedição = 5; 
 
-for(let i = 0; i < nVezes; ++i){
-    let coluna = caract;
+for(let index = 0; index < nVezesDeRepedição; index += 1){
+     caracteristica += "*";
 }
-//repedi x vezes a linha
-let linha = 0;
-for(let index = 0; index < nVezes; ++index){
-    let resultado = 
-    console.log(resultado);
+for(let indexSegundo = 0; indexSegundo < nVezesDeRepedição; indexSegundo += 1){
+    console.log(caracteristica);
 }
 
 
 
-
-
-
-//for(let index = 0; index < n; ++index) {
-//   let coluna = n 
-//}
