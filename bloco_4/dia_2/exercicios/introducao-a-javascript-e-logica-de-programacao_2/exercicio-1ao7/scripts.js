26cd8dd19d9724bae46c9333100a8f3d5f6c34fc
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-26 02:29:20 -0300

    Terminou o exercicio do 1 ao 7

--- bloco_4/dia_2/exercicios/introducao-a-javascript-e-logica-de-programacao_2/exercicio-1ao7/scripts.js
+++ bloco_4/dia_2/exercicios/introducao-a-javascript-e-logica-de-programacao_2/exercicio-1ao7/scripts.js
@@ -33,15 +33,59 @@ if (resultadoFinal > 20) {
     console.log("valor menor ou igual a 20");
 }
 
-//Exercicio 5
-//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+//Exercicio 5 - Ajuda slack 
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeroMax = 0;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(let index = 0; index < numeros.length; index =+ 1){
+    let numeroTwo = numeros[index];
+    console.log(numeroTwo);
+    if(numeroTwo > numeroMax) {
+        numeroMax = numeroTwo;
+    } else {
 
-for(let index = 0; index < numbers.length; index =+ 1){
-    let numeroAtual = numbers[index]
-    console.log(numeroAtual);
+    }
 }
 
-console.log(numeroAtual);
+console.log(numeroMax);
+// outra forma de executar o arquivo 5
+
+// Exercicio 6
+
+let arraynumbrs = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numero = 0;
+let hitNumerosDeImpar = 0;
+
+
+for(let index = 0; index < arraynumbrs.length; index += 1){
+    let numero = arraynumbrs[index];
+    if(numero % 2 ==! 0) {
+      hitNumerosDeImpar += 1;
+    } else {
+        false;
+    }
+}
+
+if (hitNumerosDeImpar === 0) {
+    console.log("nenhum valor ímpar encontrado");
+    break;
+} 
+
+console.log(hitNumerosDeImpar);
+
+//exercicio 7
+
+let numerosExercicios = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeroMini = 0;
+
+for(let index = 0; index < numerosExercicios.length; index =+ 1){
+    let numeroResultado = numerosExercicios[index];
+    if(numeroResultado < numeroMini) {
+        numeroMini = numeroResultado;
+    } else {
+
+    }
+}
+
+console.log(numeroMini);
 
