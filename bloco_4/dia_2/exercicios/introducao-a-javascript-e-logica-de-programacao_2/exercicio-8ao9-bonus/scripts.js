4a68d1855fa6b53d2a164c3740b577765a2b741b
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-26 02:46:32 -0300

    Finalizado exercicio oito

---
+++ bloco_4/dia_2/exercicios/introducao-a-javascript-e-logica-de-programacao_2/exercicio-8ao9-bonus/scripts.js
@@ -0,0 +1,11 @@
+//Exercicio oito 
+let numbers = 25;
+let arrayNumbers = [];
+
+for(let index = 1; index <= numbers; index += 1){
+    arrayNumbers.push(index);
+}
+
+console.log(arrayNumbers);
+
+//Exercicio nove 
