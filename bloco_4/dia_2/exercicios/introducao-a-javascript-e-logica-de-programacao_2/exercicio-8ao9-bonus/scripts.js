d118ecd969df31ccf9cd8b3786c5f6e91bb675f7
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-26 12:56:37 -0300

    Exericio bonus iniciado

--- bloco_4/dia_2/exercicios/introducao-a-javascript-e-logica-de-programacao_2/exercicio-8ao9-bonus/scripts.js
+++ bloco_4/dia_2/exercicios/introducao-a-javascript-e-logica-de-programacao_2/exercicio-8ao9-bonus/scripts.js
@@ -15,3 +15,31 @@ for(let indexDivisão = 0; indexDivisão < arrayNumbers.length; indexDivisão +=
 }
 
 //Bonus 
+//Exercicio 1
+ let numbers = [0,3,2,5,4,6,1,7];
+ 
+ for(let index = 1; index < numbers.length; index += 1){
+     for (let secondIndex = 0; secondIndex < index; secondIndex += 1){
+         if(numbers[index] < numbers[secondIndex]){
+             let position = numbers[index];
+             numbers[index] = numbers[secondIndex];
+             numbers[secondIndex] = position;
+         }
+     }
+     console.log(position);
+ }
+
+ //Exercicio 2
+ 
+ for(let index = 1; index < numbers.length; index += 1){
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1){
+        if(numbers[index] > numbers[secondIndex]){
+            let positionOrd = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = positionOrd;
+        }
+    }
+    console.log(positionOrd);
+}
+
+
