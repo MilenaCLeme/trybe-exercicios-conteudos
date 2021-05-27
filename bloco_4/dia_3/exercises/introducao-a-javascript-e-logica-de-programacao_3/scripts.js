301d47144b728712b5f306a0e9144c73dadc7ccd
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-27 13:44:09 -0300

    Organização das pastas

---
+++ bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts.js
@@ -0,0 +1,48 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n 
+
+//Qual a variavel que precisa imprimir 
+let caracteristica = ""
+
+//repedi x vezes a coluna 
+let nVezesDeRepedição = 5; 
+
+for(let index = 0; index < nVezesDeRepedição; index += 1){
+     caracteristica += "*";
+}
+for(let indexSegundo = 0; indexSegundo < nVezesDeRepedição; indexSegundo += 1){
+    console.log(caracteristica);
+}
+
+//2- 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+//Qual a variavel que precisa imprimir
+let caract = "";
+
+//repedir x vezes
+let repedir = 5;
+
+for(let index = 0; index < repedir; index += 1){
+    caract += "*";
+    console.log(caract);
+}
+
+//3 -Agora inverta o lado do triângulo. 
+//valores que precisaram ser imprimidos 
+let caractr = "rrrrr";
+let caractestilo = "*";
+let resultadofinal = 0;
+let resultado = "";
+
+//quantas vezes 
+let numeroDeVezes = 5;
+
+for(let index = 0; index < numeroDeVezes; index += 1){
+    resultado += caractr;
+    console.log(resultado);
+}
+
+
+//4- Depois, faça uma pirâmide com n asteriscos de base:
+
+
+
