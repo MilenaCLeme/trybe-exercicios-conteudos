23a7eda1d456669cf1c4138bba9cdd2cbd94d180
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-28 01:51:08 -0300

    Imprimiu a informaçao do objeto da tela

---
+++ bloco_4/dia_4/conteudo/script.js
@@ -0,0 +1,14 @@
+//Conteudo de Objetos;
+
+let player = {
+    name: "Marta",
+    lastaName: "Silva",
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3
+    }
+};
+
+console.log("A jogadora " + player.name + " " + player.lastaName + " tem " + player.age + " anos de idade");
+
