417d87fbe4c6055b6f3864d1c2bf51a2bb103365
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-13 02:17:56 -0300

    Iniciou a segunda etapa do exercicio do bloco 5 dia 2

--- bloco_4/dia_4/exercicios/script.js
+++ bloco_4/dia_4/exercicios/script.js
@@ -70,6 +70,8 @@ let infoTwo = {
       console.log(repedir);
   }
 
+  //Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
 
+  
 
   
