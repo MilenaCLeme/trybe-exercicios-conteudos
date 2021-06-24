93d6d3fa190bddbeeb4bf44dbdee350fbe6e6bf4
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-24 00:28:15 -0300

    identificando corresoes

--- bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts.js
+++ bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts.js
@@ -64,11 +64,53 @@ for(let index = 1; index <= repedir; index += 1){
 
 
 //  Depois, faça uma pirâmide com n asteriscos de base:
+// base do codigo refazer ! 
+let n = 5;
+let symbol = '*';
+let inputLine = '';
 
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
 
 
 
 //Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+let n = 7;
+let meio = (n + 1) / 2;
+let controleEsquerda = meio;
+let controleDireita = meio;
+let estrela = '*';
+
+for (let linha = 1; linha <= meio; linha += 1) {
+    let linhas = '';
+    for (let coluna = 1; coluna <= n; coluna += 1) {
+        if (coluna == controleEsquerda || coluna == controleDireita || linha == meio) { // Porque tem essa Linha ? 
+            linhas += estrela;
+        } else {
+            linhas += ' ';
+        }
+    }
+    controleEsquerda -= 1;
+    controleDireita += 1;
+    console.log(linhas)
+}
+
+
 
 //Faça um programa que diz se um número definido numa variável é primo ou não.
 
