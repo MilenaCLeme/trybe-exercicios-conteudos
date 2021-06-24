83968ed9fd1bce5e829d1210a0892db89952e28b
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-23 23:42:11 -0300

    bloco 8 de exercicio

--- bloco_8/dia_1/exercicio/exercicio.js
+++ bloco_8/dia_1/exercicio/exercicio.js
@@ -117,12 +117,16 @@ const danoDoWarrior = () => {
 //Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 //O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
 //A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+const manaMenos = mage.mana
+let diminuicao = 0
+
 
 const danoDoMago = () => {
-  let danoMagicoMax = mage.intelligence * 2
-  let danos = Math.floor(Math.random() * (danoMagicoMax - mage.intelligence) + danoMagicoMax) 
-  let mana = 
+  let danoMagicoMax = mage.intelligence * 2; 
   const objDanoManaGasta = {
-    dano: danos,
+    dano: Math.floor(Math.random() * (danoMagicoMax - mage.intelligence) + danoMagicoMax),
   }
+  return objDanoManaGasta
 }
+
+console.log(danoDoMago())
