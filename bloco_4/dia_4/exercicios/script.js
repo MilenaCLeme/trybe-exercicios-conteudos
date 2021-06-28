3250b2cdc4781f1bbd03c3621596b71eb515d8aa
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-27 23:43:23 -0300

    terminou exercicio do bonus 3

--- bloco_4/dia_4/exercicios/script.js
+++ bloco_4/dia_4/exercicios/script.js
@@ -138,3 +138,44 @@ function arrayOfNumbers(vector) {
 
 arrayOfNumbers(novo)
 
+//A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 } quando passado como argumento para a função. Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+
+
+const cestas = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+const conjDeFrutas = ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'];
+
+function cestaDeFrutasComTodasAsFrutas(arrayCesta, arrayFrutas) {
+    let cesta = {}  
+    for (let index = 0; index < arrayFrutas.length; index += 1){
+        let fruta = arrayFrutas[index]
+        let soma = 0
+        for (let indexS = 0; indexS < arrayCesta.length; index += 1) {
+            if (fruta === arrayCesta){
+            soma += 1
+            }
+        }
+        cesta.fruta = soma
+    }
+    console.log(cesta)
+}
+
+cestaDeFrutasComTodasAsFrutas(cestas, conjDeFrutas)
