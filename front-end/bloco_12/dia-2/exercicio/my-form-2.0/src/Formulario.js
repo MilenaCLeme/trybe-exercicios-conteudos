dc6b8139c87287d6708acaa039e8a4397e4abdfa
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-04 16:16:58 -0300

    formulario

--- front-end/bloco_12/dia-2/exercicio/my-form-2.0/src/Formulario.js
+++ front-end/bloco_12/dia-2/exercicio/my-form-2.0/src/Formulario.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { cpfMask } from "./Func";
 
 class Formulario extends React.Component {
   constructor(props) {
@@ -6,22 +7,50 @@ class Formulario extends React.Component {
 
     this.state = {
       nome: '',
+      email: '',
+      cpf: '',
     }
 
     this.handlerChangeName = this.handlerChangeName.bind(this);
+    this.handlerChangeEmail = this.handlerChangeEmail.bind(this);
+    this.handlerChangeCpf = this.handlerChangeCpf.bind(this);
   }
 
-  handlerChangeName({target}) {
-    const { value } = target;
+  handlerChangeName(event) {
+    const { value } = event.target;
     if (value.length <= 40) {
       this.setState({
         nome: value.toUpperCase(),
       })
+    } else {
+      event.preventDefault()
+    }
+  }
+
+  handlerChangeEmail(event) {
+    const { value } = event.target;
+    if (value.length <= 50) {
+      this.setState({
+        email: value,
+      })
+    } else {
+      event.preventDefault()
+    }
+  }
+
+  handlerChangeCpf(event) {
+    const {value} = event.target
+    if (value.length <= 14) {
+      this.setState({
+        cpf: cpfMask(value)
+      })
+    } else {
+      event.preventDefault();
     }
   }
 
   render() {
-    const { name } = this.state;
+    const { nome, email, cpf } = this.state;
     return (
       <form>
         <fieldset>
@@ -32,7 +61,27 @@ class Formulario extends React.Component {
               id="nome"
               name="nome"
               onChange={this.handlerChangeName}
-              value={name}
+              value={ nome }
+            />
+          </label>
+          <label htmlFor="email">
+            EMAIL:
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={ email }
+              onChange={this.handlerChangeEmail}
+            />
+          </label>
+          <label htmlFor="cpf">
+            CPF:
+            <input
+              type="text"
+              id="cpf"
+              name="cpf"
+              value={ cpf }
+              onChange={this.handlerChangeCpf}
             />
           </label>
         </fieldset>
