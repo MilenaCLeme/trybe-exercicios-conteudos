a7ed14bfa0105b6f3ea22445e04e4b5038b26fe0
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-06 03:09:09 -0300

    Formulario

--- front-end/bloco_12/dia-2/exercicio/my-form-2.0/src/Formulario.js
+++ front-end/bloco_12/dia-2/exercicio/my-form-2.0/src/Formulario.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { cpfMask } from "./Func";
+import { states } from './estados';
+import Curriculo from  './Curriculo';
 
 class Formulario extends React.Component {
   constructor(props) {
@@ -9,11 +11,27 @@ class Formulario extends React.Component {
       nome: '',
       email: '',
       cpf: '',
+      endereco: '',
+      cidade: '',
+      estado: 'Acre',
+      tipo: '',
+      resumo: '',
+      cargo: '',
+      descricao: '',
+      enviar: false,
     }
 
     this.handlerChangeName = this.handlerChangeName.bind(this);
     this.handlerChangeEmail = this.handlerChangeEmail.bind(this);
     this.handlerChangeCpf = this.handlerChangeCpf.bind(this);
+    this.handlerChangeEndereco = this.handlerChangeEndereco.bind(this);
+    this.onBlurHandler = this.onBlurHandler.bind(this);
+    this.handlerChangeEstado = this.handlerChangeEstado.bind(this);
+    this.handlerChangeTipo = this.handlerChangeTipo.bind(this);
+    this.handlerChangeResumo = this.handlerChangeResumo.bind(this);
+    this.handlerChangeCargo = this.handlerChangeCargo.bind(this);
+    this.handlerChangeDescricao = this.handlerChangeDescricao.bind(this);
+    this.PegarInformação = this.PegarInformação.bind(this);
   }
 
   handlerChangeName(event) {
@@ -49,11 +67,109 @@ class Formulario extends React.Component {
     }
   }
 
+  handlerChangeEndereco(event) {
+    const { value } = event.target;
+    if (value.length <= 200) {
+      this.setState({
+        endereco: value.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z\s])/g, ''),
+      })
+    }
+  }
+
+  onBlurHandler(event) {
+    const { value } = event.target;
+    if (value.length <= 28) {
+      this.setState({
+        cidade: value,
+      });
+    }
+  }
+
+  handlerChangeEstado(event) {
+    const { value } = event.target;
+    this.setState({
+      estado: value,
+    });
+  }
+
+  handlerChangeTipo({target}) {
+    const { name } = target;
+    this.setState({
+      tipo: name,
+    });
+  }
+
+  handlerChangeResumo({target}) {
+    const { value } = target;
+    if (value.length <= 1000 ) {
+      this.setState({
+        resumo: value,
+      });
+    }
+  }
+
+  handlerChangeCargo({target}) {
+    const { value } = target;
+    if (value.length <= 40) {
+      this.setState({
+        cargo: value,
+      });
+    }
+  }
+
+  handlerChangeDescricao({target}) {
+    const { value } = target;
+    if (value.length <= 500) {
+      this.setState({
+        descricao: value,
+      });
+    }
+  }
+
+  PegarInformação() {
+    this.setState({
+      enviar: true,
+    });
+  }
+
+  funcaoDaDiv() {
+    const { 
+      nome,
+      email,
+      cpf,
+      endereco,
+      cidade,
+      estado,
+      tipo,
+      resumo,
+      cargo,
+      descricao,
+      enviar,
+    } = this.state;
+    if (enviar === true ) {
+      return (
+        <div>
+          <p>{nome}</p>
+          <p>{email}</p>
+          <p>{cpf}</p>
+          <p>{endereco}</p>
+          <p>{cidade}</p>
+          <p>{estado}</p>
+          <p>{tipo}</p>
+          <p>{resumo}</p>
+          <p>{cargo}</p>
+          <p>{descricao}</p>
+        </div>
+      )
+    }
+  }
+
   render() {
-    const { nome, email, cpf } = this.state;
+    const { nome, email, cpf, endereco, cidade, estado, tipo } = this.state;
     return (
       <form>
         <fieldset>
+          <h5>Dados Pessoais:</h5>
           <label htmlFor="nome">
             NOME:
             <input
@@ -84,6 +200,64 @@ class Formulario extends React.Component {
               onChange={this.handlerChangeCpf}
             />
           </label>
+          <label htmlFor="endereco">
+            Endereço:
+            <input
+              type="text"
+              id="endereco"
+              name="endereco"
+              value={ endereco }
+              onChange={this.handlerChangeEndereco}
+            />
+          </label>
+          <label htmlFor="cidade">
+            Cidade:
+            <input
+              type="text"
+              id="cidade"
+              name="cidade"
+              onChange={this.onBlurHandler}
+              value={cidade}
+            />
+          </label>
+          <label>
+            Estados:
+            <select onChange={this.handlerChangeEstado} value={estado}>
+              {
+                Object.values(states).map((element) => <option value={element}>{element}</option> )
+              }
+            </select>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="Casa"
+              checked={tipo === 'Casa'}
+              onChange={this.handlerChangeTipo}
+            />
+            Casa
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="Apartamento"
+              onChange={this.handlerChangeTipo}
+              checked={tipo === 'Apartamento'}
+            />
+            Apartamento
+          </label>
+        </fieldset>
+        <Curriculo
+          handlerChangeResumo={this.handlerChangeResumo}
+          handlerChangeCargo={this.handlerChangeCargo}
+          handlerChangeDescricao={this.handlerChangeDescricao}
+          resumo={this.state.resumo}
+          cargo={this.state.cargo}
+          descricao={this.state.descricao}
+        />
+        <fieldset>
+          <button type="button" onClick={() => this.PegarInformação() }>Enviar</button>
+          { this.funcaoDaDiv() }
         </fieldset>
       </form>
     );
